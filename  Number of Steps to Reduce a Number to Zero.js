47552853781a54c4ff5818525b1a925e73dd4e6f
yamachan0625 <napoli.080625@gmail.com>
2021-02-07 02:21:55 +0900

    Create  Number of Steps to Reduce a Number to Zero.js

---
+++  Number of Steps to Reduce a Number to Zero.js
@@ -0,0 +1,18 @@
+// Input: num = 14
+// Output: 6
+// Explanation:
+// Step 1) 14 is even; divide by 2 and obtain 7.
+// Step 2) 7 is odd; subtract 1 and obtain 6.
+// Step 3) 6 is even; divide by 2 and obtain 3.
+// Step 4) 3 is odd; subtract 1 and obtain 2.
+// Step 5) 2 is even; divide by 2 and obtain 1.
+// Step 6) 1 is odd; subtract 1 and obtain 0.
+
+var numberOfSteps = function (num) {
+  let count = 0;
+  while (num > 0) {
+    num % 2 === 0 ? (num /= 2) : (num -= 1);
+    count++;
+  }
+  return count;
+};
